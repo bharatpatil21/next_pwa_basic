3f2fe55b2b4cc711c6600da78a57923fcce9fe04
bharat_patil <patilbharat09@gmail.com>
2020-04-21 12:12:47 +0530

    Added new route and database

--- server/index.js
+++ server/index.js
@@ -1,30 +1,60 @@
 const express = require("express");
+const server = express();
 const next = require("next");
+let _ = require("lodash");
+var SwaggerExpress = require("swagger-express-mw");
 
-const port = process.env.PORT || 80;
-const dev = process.env.NODE_ENV !== "production";
+// ENV Config
+const devConfig = require("./config/dev_config.json");
+const prodConfig = require("./config/prod_config.json");
+
+const dev = process.env.NODE_ENV === "production"
+    ? _.extend(server.config, prodConfig)
+    : _.extend(server.config, devConfig);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+var config = {
+  appRoot: __dirname, // required config
+};
 
 app
   .prepare()
   .then(() => {
-    const port = process.env.PORT || 3000;
-    const server = express();
-    const showRoutes = require("./routes/index.js");
+    SwaggerExpress.create(config, function (err, swaggerExpress) {
+      if (err) {
+        throw err;
+      }
 
-    server.use("/api", showRoutes(server));
+      _.extend(server.config, swaggerExpress.runner.config);
 
-    server.get("*", (req, res) => {
-      return handle(req, res);
+      // Database connection
+      require("./middlewares/dbconnect");
+
+      // Install middleware
+      swaggerExpress.register(server);
+
+      server.get("*", (req, res) => {
+        return handle(req, res);
+      });
+
+      const port = process.env.PORT || 3000;
+      server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on ${port}`);
+      });
     });
+    // const showRoutes = require("./routes/index.js");
 
-    server.listen(port, err => {
-      if (err) throw err;
-      console.log(`> Ready on ${port}`);
-    });
+    // server.use("/api", showRoutes(server));
+
+    // server.get("*", (req, res) => {
+    //   return handle(req, res);
+    // });
   })
-  .catch(ex => {
+  .catch((ex) => {
     console.error(ex.stack);
     process.exit(1);
   });
