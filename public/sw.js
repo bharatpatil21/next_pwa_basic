9e59e82e95bcf82cb0830dce85326f840aa050cf
Marhatta <vishal.marhatta@gmail.com>
2020-04-22 19:24:25 +0530

    :label: tsConfig changes

--- public/sw.js
+++ public/sw.js
@@ -124,10 +124,10 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "82dca635a629d8ab38c3ad85b2ad65a2"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "6a18b6897ceee131b4a3a5bafdf4a01c"
+    "revision": "b51aa9f2b4caa103a3ce8f6e75fceed0"
   }, {
     "url": "/_next/static/runtime/webpack.js.map",
-    "revision": "cfc0160848d3c5cb80f6ebf45ec362cd"
+    "revision": "3c0ff30417005d1a7b8b09e637ca099e"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
