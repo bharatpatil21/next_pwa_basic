f9a787251b4f934b811e132da4c9770af2cbf068
Marhatta <vishal.marhatta@gmail.com>
2020-04-24 17:23:50 +0530

    :white_check_mark: Added public folder

--- worker/index.js
+++ worker/index.js
@@ -6,8 +6,8 @@ util()
 
 
 
-const CACHE_STATIC_NAME = 'static-v28';
-const CACHE_DYNAMIC_NAME = 'dynamic-v6';
+const CACHE_STATIC_NAME = 'static-v29';
+const CACHE_DYNAMIC_NAME = 'dynamic-v7';
 const INDEXDB_DYNAMIC_NAME = "linkup-db";
 const STATIC_FILES = [
     '/',
