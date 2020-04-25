50e7eff72b48b3b07cecff3114886891e71e66a2
bharat_patil <patilbharat09@gmail.com>
2020-04-25 12:15:51 +0530

    add check to load config

--- server/index.js
+++ server/index.js
@@ -8,15 +8,14 @@ var SwaggerExpress = require("swagger-express-mw");
 const devConfig = require("./config/dev_config.json");
 const prodConfig = require("./config/prod_config.json");
 
-const dev = process.env.NODE_ENV !== "production";
-//     ? _.extend(server.config, prodConfig)
-//     : _.extend(server.config, devConfig);
+const dev = process.env.NODE_ENV !== "production"
+const serverConfig = dev ? devConfig : prodConfig;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const app = next({ dev });
+const app = next({ dev, conf: serverConfig });
 const handle = app.getRequestHandler();
 
-var config = {
+var config = server.config = {
   appRoot: __dirname, // required config
 };
 
@@ -28,7 +27,12 @@ app
         throw err;
       }
 
-      _.extend(server.config, swaggerExpress.runner.config);
+      _.assignIn(server.config, swaggerExpress.runner.config);
+      server.config.debug = app.nextConfig.serverConfig.debug;
+      server.config.logger = {
+        dir: app.nextConfig.serverConfig.logDir,
+        level: app.nextConfig.serverConfig.logLevel
+      };
 
       // Database connection
       require("./middlewares/dbconnect");
